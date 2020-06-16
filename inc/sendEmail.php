55f04639e507d3f5f5e685da5728d789131189c4
sunnywralph <sunnywralph@gmail.com>
2020-06-16 11:11:40 +0530

    Edited PHP file

--- inc/sendEmail.php
+++ inc/sendEmail.php
@@ -20,8 +20,8 @@ if($_POST) {
 		$error['email'] = "Please enter a valid email address.";
 	}
 	// Check Message
-	if (strlen($contact_message) < 15) {
-		$error['message'] = "Please enter your message. It should have at least 15 characters.";
+	if (strlen($contact_message) < 10) {
+		$error['message'] = "Please enter your message. It should have at least 10 characters.";
 	}
    // Subject
 	if ($subject == '') { $subject = "Contact Form Submission"; }
